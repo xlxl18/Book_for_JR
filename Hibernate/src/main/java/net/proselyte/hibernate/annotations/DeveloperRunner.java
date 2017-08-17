1ee689c9d516d693e13c90e6174bb6ff7994debd
andrew-okhrimchuk <andrew456gold@gmail.com>
2017-08-17 13:28:55 +0300

    1 save(не работают норм аннотации, не знаю от чего)

---
+++ Hibernate/src/main/java/net/proselyte/hibernate/annotations/DeveloperRunner.java
@@ -0,0 +1,84 @@
+package net.proselyte.hibernate.annotations;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.cfg.Configuration;
+
+
+import java.util.List;
+
+public class DeveloperRunner {
+    private static SessionFactory sessionFactory;
+
+    public static void main(String[] args) {
+        sessionFactory = new Configuration().configure().buildSessionFactory();
+
+        DeveloperRunner developerRunner = new DeveloperRunner();
+
+        System.out.println("Adding Developer's records to the database");
+        Integer developerId1 = developerRunner.addDeveloper("Proselyte", "Developer", "Java Developer", 2);
+        Integer developerId2 = developerRunner.addDeveloper("Some", "Developer", "C++ Developer", 2);
+        Integer developerId3 = developerRunner.addDeveloper("Peter", "Team Lead", "Java Team Lead", 6);
+
+        System.out.println("List of Developers:");
+        developerRunner.listDevelopers();
+
+        System.out.println("Removing \'Some Developer\' and updating \'Proselyte Developer\''s experience:");
+        developerRunner.removeDeveloper(developerId2);
+        developerRunner.updateDeveloper(developerId1, 3);
+
+        System.out.println("Final list of Developers:");
+        developerRunner.listDevelopers();
+    }
+
+    public Integer addDeveloper(String firstName, String lastName, String specialty, int experience) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = null;
+        Integer developerId = null;
+
+        transaction = session.beginTransaction();
+        Developer developer = new Developer(firstName, lastName, specialty, experience);
+        developerId = (Integer) session.save(developer);
+        transaction.commit();
+        session.close();
+        return developerId;
+    }
+
+    public void listDevelopers() {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = null;
+
+        transaction = session.beginTransaction();
+        List<Developer> developers = session.createQuery("FROM Developer").list();
+        for (Developer developer : developers) {
+            System.out.println(developer);
+            System.out.println("\n================\n");
+        }
+        session.close();
+    }
+
+    public void updateDeveloper(int developerId, int experience) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = null;
+
+        transaction = session.beginTransaction();
+        Developer developer = (Developer) session.get(Developer.class, developerId);
+        developer.setExperience(experience);
+        session.update(developer);
+        transaction.commit();
+        session.close();
+    }
+
+    public void removeDeveloper(int developerId) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = null;
+
+        transaction = session.beginTransaction();
+        Developer developer = (Developer) session.get(Developer.class, developerId);
+        session.delete(developer);
+        transaction.commit();
+        session.close();
+    }
+}
