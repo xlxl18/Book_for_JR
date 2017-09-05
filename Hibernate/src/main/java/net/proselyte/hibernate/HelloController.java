fe945808961fae730449fc86b83d2c46138309b6
andrew-okhrimchuk <andrew456gold@gmail.com>
2017-09-05 15:16:25 +0300

    Педдинг, и поиск работают отлично.
    - не работает правильное отображение значения isAdmin // исправлю позже
    - ошибка в заполнении имени по-русски, знаю как поправить, все потом..
    - ошибка в вводе даты, читает только текст без точек.
    
                                                              Signed-off-by: andrew-okhrimchuk <andrew456gold@gmail.com>
    
    Signed-off-by: andrew-okhrimchuk <andrew456gold@gmail.com>

--- Hibernate/src/main/java/net/proselyte/hibernate/HelloController.java
+++ Hibernate/src/main/java/net/proselyte/hibernate/HelloController.java
@@ -52,37 +52,38 @@ public class HelloController {
     @RequestMapping(value = "/springPaginationDataTables.web", method = RequestMethod.GET, produces = "application/json")
     public @ResponseBody String springPaginationDataTables(HttpServletRequest request) throws IOException {
 
+        //Fetch Page display length
+        // Получить Длительность отображения страницы
+        Integer pageDisplayLength = Integer.valueOf(request.getParameter("iDisplayLength"));
         //Fetch the page number from client
         // Получить номер страницы с клиента
         Integer pageNumber = 0;
         if (null != request.getParameter("iDisplayStart"))
-            pageNumber = (Integer.valueOf(request.getParameter("iDisplayStart")) / 10) + 1;
+            System.out.print("request.getParameter = " + request.getParameter("iDisplayStart"));
+
+        {  pageNumber = (Integer.valueOf(request.getParameter("iDisplayStart")) / pageDisplayLength) + 1;
+         // pageNumber = (Integer.valueOf(request.getParameter("iDisplayStart")) / Integer.valueOf(request.getParameter("iDisplayStart"))) + 1;
+          //  pageNumber =  pageNumber + 1;
+        }
+        System.out.println(":  pageNumber = " + pageNumber);
 
         //Fetch search parameter
         // Выбор параметра поиска
         String searchParameter = request.getParameter("sSearch");
 
-        //Fetch Page display length
-        // Получить Длительность отображения страницы
-        Integer pageDisplayLength = Integer.valueOf(request.getParameter("iDisplayLength"));
-
         //Create page list data
         // Создание данных списка страниц
-        List<User> personsList = createPaginationData(pageNumber, pageDisplayLength);
-      //  List<User> personsList = createPaginationDataOnSearchParameter(pageNumber, pageDisplayLength, searchParameter);
+       // List<User> personsList = createPaginationData(pageNumber, pageDisplayLength);
+        List<User> personsList = createPaginationDataOnSearchParameter(pageNumber, pageDisplayLength, searchParameter);
 
         //Here is server side pagination logic. Based on the page number you could make call
         //to the data base create new list and send back to the client. For demo I am shuffling
         //the same list to show data randomly
         // Здесь представлена логика разбиения на страницы на стороне сервера. На основании номера страницы,
         // которую вы могли бы позвонить в базу данных создаем новый список и отправляем обратно клиенту.
-        // Для демонстрации я перетасовываю тот же список для случайного отображения данных
-
 
         //Search functionality: Returns filtered list based on search parameter
         // Функция поиска: возвращает список фильтров на основе параметра поиска
-  //      personsList = getListBasedOnSearchParameter(searchParameter,userService.listUsersReturn());
-
 
         UserJsonObject userJsonObject = new UserJsonObject();
         //Set Total display record
@@ -90,7 +91,7 @@ public class HelloController {
         userJsonObject.setiTotalDisplayRecords(userService.getCountUsers());
         //Set Total record
         // Встановити загальну кількість записів после фильтрации
-        userJsonObject.setiTotalRecords(userService.getCountUsers());
+        userJsonObject.setiTotalRecords(personsList.size());
         userJsonObject.setAaData(personsList);
 
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
@@ -98,33 +99,10 @@ public class HelloController {
 
         return json2;
     }
-/*
-    private List<User> getListBasedOnSearchParameter(String searchParameter,List<User> personsList) {
-
-        if (null != searchParameter && !searchParameter.equals("")) {
-            List<User> personsListForSearch = new ArrayList<User>();
-            searchParameter = searchParameter.toUpperCase();
-            for (User person : personsList) {
-                if (person.getName().toUpperCase().indexOf(searchParameter)!= -1 ) {
-                    personsListForSearch.add(person);
-                }
-
-            }
-            personsList = personsListForSearch;
-            personsListForSearch = null;
-        }
-        return personsList;
-    }
- */
-    private List<User> createPaginationData(Integer pageNumber, Integer pageDisplayLength) {
-        int start = (pageNumber-1)*pageDisplayLength ;
-        int maxRows = start + pageDisplayLength;
-        return userService.listUsersReturnFROM(start, maxRows, null);
-    }
 
     private List<User> createPaginationDataOnSearchParameter(Integer pageNumber, Integer pageDisplayLength, String searchParameter) {
         int start = (pageNumber-1)*pageDisplayLength ;
-        int maxRows = start + pageDisplayLength-1;
+        int maxRows = pageDisplayLength;
         return userService.listUsersReturnFROM(start, maxRows, searchParameter);
     }
 
@@ -153,7 +131,7 @@ public class HelloController {
     public ModelAndView editUser(@RequestParam int id, @ModelAttribute User user) {
         ModelAndView mv = new ModelAndView();
         user = userService.getUser(id);
-        System.out.println(user.getIsAdmin());
+
         mv.addObject("message", "Edit User");
         mv.addObject("message2", "editUser");
         mv.setViewName("adduser");//страничка jsp которую я вызываю
@@ -175,11 +153,6 @@ public class HelloController {
         return new ModelAndView("userList", "userList", userList);
     }
 
-    @RequestMapping(value = "/getData", method = RequestMethod.GET)
-    public ResponseEntity<Integer> getData() {
-        int count = userService.getCountUsers();
-        System.out.println(count);
-        return new ResponseEntity<Integer>(55, HttpStatus.OK);
-    }
+
 
 }
