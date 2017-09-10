76d07ebfbd930acab98fe97ecbeb9f7eeb2389f6
andrew-okhrimchuk <andrew456gold@gmail.com>
2017-09-10 23:10:54 +0300

    add PoolProperty in DataBase. All OK!
                                                              Signed-off-by: andrew-okhrimchuk <andrew456gold@gmail.com>
    
    Signed-off-by: andrew-okhrimchuk <andrew456gold@gmail.com>

--- Hibernate/src/main/java/net/proselyte/hibernate/config/PersistenceJPAConfig.java
+++ Hibernate/src/main/java/net/proselyte/hibernate/config/PersistenceJPAConfig.java
@@ -1,5 +1,6 @@
 package net.proselyte.hibernate.config;
 import net.proselyte.hibernate.dao.UserJpiImpl;
+import org.apache.tomcat.jdbc.pool.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -11,9 +12,11 @@ import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.apache.tomcat.jdbc.pool.PoolProperties;
 import javax.persistence.EntityManagerFactory;
-import javax.sql.DataSource;
+//import javax.sql.DataSource;
 import java.util.Properties;
+
 
 @Configuration
 @ComponentScan({ "net.proselyte.hibernate" })// пакеты, в которых будет осуществлён поиск классов с описанием сущностей
@@ -34,13 +37,32 @@ public class PersistenceJPAConfig{
 
     @Bean // - источник данных
     public DataSource dataSource(){
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/test");
-        dataSource.setUsername( "root" );
-        dataSource.setPassword( "root" );
-
-        return dataSource;
+        PoolProperties p = new PoolProperties();
+        p.setUrl("jdbc:mysql://localhost:3306/test");
+        p.setDriverClassName("com.mysql.jdbc.Driver");
+        p.setUsername("root");
+        p.setPassword("root");
+        p.setJmxEnabled(true);
+        p.setTestWhileIdle(false);
+        p.setTestOnBorrow(true);
+        p.setValidationQuery("SELECT 1");
+        p.setTestOnReturn(false);
+        p.setValidationInterval(30000);
+        p.setTimeBetweenEvictionRunsMillis(30000);
+        p.setMaxActive(100);
+        p.setInitialSize(10);
+        p.setMaxWait(10000);
+        p.setRemoveAbandonedTimeout(60);
+        p.setMinEvictableIdleTimeMillis(30000);
+        p.setMinIdle(10);
+        p.setLogAbandoned(true);
+        p.setRemoveAbandoned(true);
+        p.setJdbcInterceptors(
+                "org.apache.tomcat.jdbc.pool.interceptor.ConnectionState;"+
+                        "org.apache.tomcat.jdbc.pool.interceptor.StatementFinalizer");
+        DataSource datasource = new DataSource();
+        datasource.setPoolProperties(p);
+        return datasource ;
     }
     // Declare a transaction manager
     @Bean
@@ -70,7 +92,7 @@ public class PersistenceJPAConfig{
         Properties properties = new Properties();
         properties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
         properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
-
         return properties;
     }
+
 }
